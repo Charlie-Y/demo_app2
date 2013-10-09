e64ef675fc86abb632ffd98e10820d770f4af7d7
Charles Yang <charlesy@stanford.edu>
2013-10-08 22:04:16 -0700

    first commit

---
+++ stuff/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuff::Application.config.secret_token = '872c7306191834f2c38b9e550325314bac223c3b76f5ed896764a1dfbbc82408f72fd5e0e26d3de9ab9cc6cbe63de4f2b5d8d05a9ce65fe819d9886111b3951f'
